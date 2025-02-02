08b91db6da616afad0131025166270e88c139b96
keyboardAnt <nadav.timor@weizmann.ac.il>
2025-02-02 21:57:47 +0200

    fix import to support the new `hf_bench` lib

--- hf_bench/benchmark.py
+++ hf_bench/benchmark.py
@@ -21,7 +21,7 @@ from huggingface_hub import login
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from transformers.generation.streamers import BaseStreamer
 
-from .config import DatasetConfig, ExperimentConfig, experiment_configs
+from hf_bench.config import DatasetConfig, ExperimentConfig, experiment_configs
 
 # ------------------------------------------------------------------------------
 # Environment & Setup
