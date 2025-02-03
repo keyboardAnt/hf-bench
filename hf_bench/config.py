ede445987a76975b06a5533139af8711eccb1e1b
jmamou <jonathan.mamou@intel.com>
2025-02-03 09:00:44 -0800

    add config

--- hf_bench/config.py
+++ hf_bench/config.py
@@ -81,6 +81,54 @@ experiment_configs = {
         ],
         temperatures=[0, 1e-7, 1],
     ),
+    "llama70b-instruct-cnndm": ExperimentConfig(
+        target="meta-llama/Llama-3.1-70B-Instruct",
+        dataset_configs=[DatasetConfig.from_path("cnn_dailymail")],
+        assistants=[
+            "meta-llama/Llama-3.1-8B-Instruct",
+            "meta-llama/Llama-3.2-3B-Instruct",
+            "meta-llama/Llama-3.2-1B-Instruct",
+            "Qwen/Qwen2.5-0.5B-Instruct",
+            "double7/vicuna-68m",
+        ],
+        temperatures=[0, 1e-7, 1],
+    ),
+    "llama8b-instruct-cnndm": ExperimentConfig(
+        target="meta-llama/Llama-3.1-8B-Instruct",
+        dataset_configs=[DatasetConfig.from_path("cnn_dailymail")],
+        assistants=[
+            "meta-llama/Llama-3.2-3B-Instruct",
+            "meta-llama/Llama-3.2-1B-Instruct",
+            "Qwen/Qwen2.5-0.5B-Instruct",
+            "double7/vicuna-68m",
+        ],
+        temperatures=[0, 1e-7, 1],
+    ),
+    "llama70b-cnndm": ExperimentConfig(
+        target="meta-llama/Llama-3.1-70B",
+        dataset_configs=[DatasetConfig.from_path("cnn_dailymail")],
+        assistants=[
+            "meta-llama/Llama-3.1-8B",
+            "meta-llama/Llama-3.2-3B",
+            "meta-llama/Llama-3.2-1B",
+            "Qwen/Qwen2.5-0.5B",
+            "double7/vicuna-68m",
+        ],
+        temperatures=[0, 1e-7, 1],
+    ),
+    "llama8b-cnndm": ExperimentConfig(
+        target="meta-llama/Llama-3.1-8B",
+        dataset_configs=[DatasetConfig.from_path("cnn_dailymail")],
+        assistants=[
+            "meta-llama/Llama-3.2-3B",
+            "meta-llama/Llama-3.2-1B",
+            "Qwen/Qwen2.5-0.5B",
+            "double7/vicuna-68m",
+        ],
+        temperatures=[0, 1e-7, 1],
+    ),
+
+
     "mixtral-8x22b-it": ExperimentConfig(
         target="mistralai/Mixtral-8x22B-Instruct-v0.1",
         dataset_configs=list(dataset_configs.values()),
