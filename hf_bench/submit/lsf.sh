5f68f50b45237af0fa804bf5840a0dba288a34c5
keyboardAnt <nadav.timor@weizmann.ac.il>
2025-02-04 19:53:43 +0200

    submit lsf with only 20gb cpu mem

--- hf_bench/submit/lsf.sh
+++ hf_bench/submit/lsf.sh
@@ -4,10 +4,10 @@ timestamp=$(date +\%Y\%m\%d_\%H\%M\%S) && bsub \
     -N \
     -q "long-gpu short-gpu risk-gpu" \
     -gpu "num=1:j_exclusive=yes:gmem=80GB" \
-    -R "rusage[mem=200GB]" \
+    -R "rusage[mem=20GB]" \
     -R "affinity[core(8)]" \
     -R "span[hosts=1]" \
     -n 1 \
-    -M 200GB \
+    -M 20GB \
     -o /home/projects/dharel/nadavt/repos/hf-bench/logs/cluster/${timestamp}_jobid_%J_benchmark.log \
     "module load miniconda/24.11_environmentally && module load CUDA/12.4.0 && conda activate hf-bench-env && python -m hf_bench.benchmark  ${@}"
