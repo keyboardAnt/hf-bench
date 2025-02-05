5f8d94cfec1b33ff6577c772fc0166a97180941a
keyboardAnt <nadav.timor@weizmann.ac.il>
2025-02-06 01:54:19 +0200

    Test: All example IDs should appear the same number of times

--- tests/test_submitted_benchmark_results.py
+++ tests/test_submitted_benchmark_results.py
@@ -13,3 +13,7 @@ def test_submitted_benchmark_results():
         col_counter = Counter(df.columns)
         for col in expected_columns:
             assert col_counter[col] == 1, f"Column {col} is missing in the dataframe or appears multiple times"
+        # Check all the example_ids are repeated the same number of times
+        example_ids_counts = df["example_id"].value_counts()
+        for example_id, count in example_ids_counts.items():
+            assert count == example_ids_counts[0], f"All example IDs should appear the same number of times.\nExample ID {example_id} appears {count} times in the dataframe.\nValue counts: {example_ids_counts=}\nFilepath: {f}"
