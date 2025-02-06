dd6444c5dd5220c19ebe3106537015d788ff0258
jmamou <jonathan.mamou@intel.com>
2025-02-06 05:12:50 -0800

    add latex table generation

--- hf_bench/analyze_vocab_overlap.py
+++ hf_bench/analyze_vocab_overlap.py
@@ -51,7 +51,7 @@ def compare_vocabularies(target_model, assistant_model, num_samples, dataset_con
         assistant_model_device='cpu',
         target_vocab_size=target_vocab_size,
     )
-    #indices = torch.arange(len(translator._assistant_to_target_input_ids))
+    
     count = torch.sum(-1 != translator._assistant_to_target_input_ids).item()
     print(f'Vocab overlap from translator: {count}')
     print(f'Translator Overlap: {round(count/vocab_size1*100,0)}')
@@ -97,23 +97,120 @@ def compare_vocabularies(target_model, assistant_model, num_samples, dataset_con
 
     print(f'Prompt Freq Overlap: {round(count/total_len*100,0)}')
 
+def generate_latex_tables(target_model, assistant_model, num_samples, dataset_config, vocab_overlap_table, dataset_overlap_table):
+    assistant_tokenizer = None
+    id = f'{target_model.split("/")[-1]} & {assistant_model.split("/")[-1]} & '
+    if id not in vocab_overlap_table:
+        # Load tokenizers
+        target_tokenizer = AutoTokenizer.from_pretrained(target_model)
+        assistant_tokenizer = AutoTokenizer.from_pretrained(assistant_model)
+    
+        # Load configurations
+        config_target = AutoConfig.from_pretrained(target_model)
+        
+        # Vocabulary sizes from config
+        target_vocab_size = config_target.vocab_size
 
-# Example usage
+        translator = AssistantToTargetTranslator(
+            target_tokenizer=target_tokenizer,
+            assistant_tokenizer=assistant_tokenizer,
+            target_vocab_size=target_vocab_size,
+        )
+        
+        T_inter_D = torch.sum(-1 != translator._assistant_to_target_input_ids).item()
+        T_inter_D_by_T = round(T_inter_D/target_vocab_size,2)
+        vocab_overlap_table[id] = f'{T_inter_D} & {T_inter_D_by_T}'
+
+    dataset_path = dataset_config.path
+    id = f'{target_model.split("/")[-1]} & {assistant_model.split("/")[-1]} & {dataset_path} & '
+    if id not in dataset_overlap_table:
+        match dataset_path:
+            case "tau/scrolls":
+                task = "long-ctx summ"
+            case "cnn_dailymail":
+                task = "summ"
+            case "openai/openai_humaneval":
+                task = "coding"
+            case _:
+                raise ValueError(
+                    f"Unknown dataset path: {dataset_path}"
+                )
+        id = f'{target_model} & {assistant_model} & {task} & {dataset_path} & '
+        if assistant_tokenizer is None:
+            assistant_tokenizer = AutoTokenizer.from_pretrained(assistant_model)
+            target_tokenizer = AutoTokenizer.from_pretrained(target_model)
+            translator = AssistantToTargetTranslator(
+                target_tokenizer=target_tokenizer,
+                assistant_tokenizer=assistant_tokenizer,
+            )
+        dataset_name = dataset_config.name
+        dataset_split = dataset_config.split
+
+        dataset = load_dataset(
+            path=dataset_path,
+            name=dataset_name,
+            split=dataset_split,
+            trust_remote_code=True,
+        )
+        ds_iterator = iter(dataset.take(num_samples))
+
+        pbar = tqdm(range(num_samples))
+        T_inter_D = 0
+        total_len = 0
+        for i in pbar:
+            match dataset_path:
+                case "tau/scrolls":
+                    prompt = f"Summarize the following text:\n{next(ds_iterator)['input']}\nSummary:\n"
+                case "cnn_dailymail":
+                    prompt = f"Summarize the following article:\n{next(ds_iterator)['article']}\nSummary:\n"
+                case "openai/openai_humaneval":
+                    dsi = next(ds_iterator)
+                    prompt = f"Implement the function so that it passes the tests.\nTests:\n{dsi['test']}\nFunction:\n{dsi['prompt']}\n\nYour code:\n"
+                case _:
+                    raise ValueError(
+                        f"Unknown dataset path: {dataset_path}"
+                    )
+            token_ids = assistant_tokenizer([prompt], add_special_tokens=False, return_tensors="pt")["input_ids"]
+            target_token_ids = translator._assistant_to_target_input_ids[token_ids]
+            T_inter_D += torch.sum(target_token_ids != -1).item()
+            total_len += target_token_ids.shape[1]
+        
+        overlap = round(T_inter_D/total_len,2)
+        dataset_overlap_table[id] = f"{overlap}"
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Analyze vocabulary overlap of specified configuration.")
-    parser.add_argument("--config_name", type=str, help="The name of the configuration to use.")
+    parser.add_argument("--config_names", type=str, nargs="*", help="The names of the configurations to use (space-separated list).")
+    parser.add_argument("--num_samples", type=int, default=50, help="Number of samples to use for analysis.")
+    parser.add_argument("--generate_latex", action="store_true", help="Generate latex tables for the overlap.")
     args = parser.parse_args()
+    if args.generate_latex:
+        vocab_overlap_table = dict()
+        dataset_overlap_table = dict()
+        args.config_names = [key for key in experiment_configs if "deepseek" in key.lower() and key != "deepseek-r1"]
 
-    config = experiment_configs.get(args.config_name)
-    if config:
-        print(f"Running experiment with target: {config.target}")
+    for config_name in args.config_names:
+        config = experiment_configs.get(config_name)
+        print(f'{config_name=}')
         for assistant_model in config.assistants:
-            num_samples = 50
             for dataset_config in config.dataset_configs:
-                compare_vocabularies(config.target, assistant_model, num_samples, dataset_config)
-    else:
-        print(f"No configuration found for {args.config_name}")
+                if args.generate_latex:
+                    generate_latex_tables(config.target, assistant_model, args.num_samples, dataset_config, vocab_overlap_table, dataset_overlap_table)
+                else:
+                    compare_vocabularies(config.target, assistant_model, args.num_samples, dataset_config)
+    
+    if args.generate_latex:
+        print("Vocab Overlap Table")
+        for k, v in sorted(vocab_overlap_table.items()):
+            k = k.replace('_', '\_')
+            print(f'{k} {v} \\\\')
+        
+        print("Dataset Overlap Table")
+        for k, v in sorted(dataset_overlap_table.items()):
+            k = k.replace('_', '\_')
+            print(f'{k} {v} \\\\')
+    
 
 
 
