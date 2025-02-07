3622876a1d16505423314060eea200ef10e82153
Nadav Timor <nadav.timor@weizmann.ac.il>
2025-02-06 22:30:16 -0500

    adds validated results that possibly appear in previous submissions (#33)
    * add results
    
    * improve test: validate results
    
    * remove example id 23 (because some methods are missing)
    
    * rm `example_id==4` (due to missing methods)
    
    * rm csvs with non-positive values
    
    * rm CATASTROPHIC (some example IDs compare only a proper subset of the methods) & duplicated

--- tests/test_submitted_benchmark_results.py
+++ tests/test_submitted_benchmark_results.py
@@ -1,12 +1,13 @@
 from collections import Counter
 import warnings
 import pandas as pd
-from hf_bench.summarize_results import list_tracked_files, get_columns
+from hf_bench.summarize_results import list_tracked_files, list_staged_files, get_columns
 
 
 def test_submitted_benchmark_results():
     """Test that the submitted benchmark results follow the correct format."""
     filepaths = list_tracked_files("benchmark_results")
+    filepaths += list_staged_files("benchmark_results")
     expected_columns = get_columns()
     print(expected_columns)
     benign_missing_example_ids_compared_to_others = [] # Some (target, dataset_path, drafter, temperature) have less unique example IDs than others
@@ -17,17 +18,17 @@ def test_submitted_benchmark_results():
         df = pd.read_csv(f)
         col_counter = Counter(df.columns)
         for col in expected_columns:
-            assert col_counter[col] == 1, f"Column {col} is missing in the dataframe or appears multiple times.\nFilepath: {f}"
+            assert col_counter[col] == 1, f"CATASTROPHIC: Column {col} is missing in the dataframe or appears multiple times.\nFilepath: {f}"
         # Check that all example IDs appear the same number of times
         columns_for_index = ["target", "dataset_path", "drafter", "temperature"]
         df_example_ids_nunique = df.groupby(columns_for_index)["example_id"].nunique()
         if df_example_ids_nunique.min() != df_example_ids_nunique.max():
-            print(f"File {f} has missing example IDs (example IDs do not appear the same number of times).")
+            print(f"BENIGN: File {f} has missing example IDs (example IDs do not appear the same number of times).")
             benign_missing_example_ids_compared_to_others.append(f)
         # Check that all example IDs appear num_of_examples times
         num_of_examples_max = df["num_of_examples"].max()
         if df_example_ids_nunique.min() != num_of_examples_max:
-            print(f"File {f} has wrong example IDs that repeat less than the declared number of times, which is num_of_examples={num_of_examples_max}.")
+            print(f"BENIGN: File {f} has wrong example IDs that repeat less than the declared number of times, which is num_of_examples={num_of_examples_max}.")
             benign_missing_example_ids_compared_to_declared.append(f)
         # Calculate the expected number of times each example ID should appear
         # This is the Cartesian product of the number of unique (target, dataset_path, drafter, temperature)
@@ -38,16 +39,16 @@ def test_submitted_benchmark_results():
         mask_positive = df_example_ids_count < expected_num_of_rows_per_example_id
         catastrophic_missing_example_ids = df_example_ids_count[mask_positive].index.get_level_values("example_id")
         if len(catastrophic_missing_example_ids) > 0:
-            print(f"File {f} has catastrophic missing example IDs. The following example IDs do not repeat the expected number of times, which is {expected_num_of_rows_per_example_id}={df['target'].nunique()}*{df['dataset_path'].nunique()}*{df['drafter'].nunique()}*{df['temperature'].nunique()}:\n{catastrophic_missing_example_ids.to_list()}")
+            print(f"CATASTROPHIC: File {f} has catastrophic missing example IDs. The following example IDs do not repeat the expected number of times, which is {expected_num_of_rows_per_example_id}={df['target'].nunique()}*{df['dataset_path'].nunique()}*{df['drafter'].nunique()}*{df['temperature'].nunique()}:\n{catastrophic_missing_example_ids.to_list()}")
             catastrophic_missing_example_ids_compared_to_others.append(f)
         # Check that all the values are strictly positive
         mask_positive = df[["new_toks", "ttft_ms", "tpot_ms", "out_toks_per_sec"]] > 0
         if not mask_positive.all().all().item():
-            print(f"File {f} has non-positive values.")
+            print(f"CATASTROPHIC: File {f} has non-positive values.")
             catastrophic_non_positive_values.append(f)
     if benign_missing_example_ids_compared_to_others:
-        warnings.warn("Some example IDs do not appear the same number of times in the following files:\n" + "\n".join(benign_missing_example_ids_compared_to_others))
+        warnings.warn("BENIGN: Some example IDs do not appear the same number of times in the following files:\n" + "\n".join(benign_missing_example_ids_compared_to_others))
     if benign_missing_example_ids_compared_to_declared:
-        warnings.warn("Some example IDs appear only %d times in the dataframe although they should appear %d times according to the num_of_examples column.\nFilepath: %s" % (df_example_ids_nunique.min(), num_of_examples_max, f))
-    assert len(catastrophic_missing_example_ids_compared_to_others) == 0, f"Some example IDs do not appear the same number of times in the following files:\n" + "\n".join(catastrophic_missing_example_ids_compared_to_others)
-    assert len(catastrophic_non_positive_values) == 0, f"Some values are non-positive in the following files:\n" + "\n".join(catastrophic_non_positive_values)
+        warnings.warn("BENIGN: Some example IDs appear only %d times in the dataframe although they should appear %d times according to the num_of_examples column.\nFilepath: %s" % (df_example_ids_nunique.min(), num_of_examples_max, f))
+    assert len(catastrophic_missing_example_ids_compared_to_others) == 0, f"CATASTROPHIC: Some example IDs compare only a proper subset of the methods in the following files:\n" + "\n".join(catastrophic_missing_example_ids_compared_to_others)
+    assert len(catastrophic_non_positive_values) == 0, f"CATASTROPHIC: Some values are non-positive in the following files:\n" + "\n".join(catastrophic_non_positive_values)
